2dd74a77d25c3e17728ce32eeda8a5c973ca1f00
Eren <eren>
2022-02-13 14:14:39 +0300

    Initial Commit

---
+++ Mooviely/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mooviely
+//
+//  Created by Eren Yürekli on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
